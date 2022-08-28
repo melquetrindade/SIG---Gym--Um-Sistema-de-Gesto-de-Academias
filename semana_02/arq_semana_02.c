b0a6423d32271c178d5274ab7b10f678be16338a
Melque Rodrigues <111925696+melquetrindade@users.noreply.github.com>
2022-08-28 13:13:28 -0300

    criando o "esqueleto" do código

--- semana_02/arq_semana_02.c
+++ semana_02/arq_semana_02.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include "biblioteca.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 ///             Universidade Federal do Rio Grande do Norte                 ///
@@ -9,16 +10,45 @@
 ///                  Disciplina DCT1106 -- Programação                      ///
 ///                  Projeto Sistema de Gestão Escolar                      ///
 ///////////////////////////////////////////////////////////////////////////////
-///                                Semana 02                                 ///
+///                                Semana 02                                ///
 ///////////////////////////////////////////////////////////////////////////////
 
+int entrada;
+int op_menu_principal;
+
 int main(void){
-
+    
     setlocale(LC_ALL,"");
-
-    menu_principal();
-    tela_sobre_o_tema();
-    tela_equipe();
+    #define true 1
+    #define false 0
+    entrada = true;
+    
+    while(entrada != false){
+        op_menu_principal = menu_principal();
+        if(op_menu_principal == 1){
+        	clientes();
+		}
+		else if(op_menu_principal == 2){
+			funcionarios();
+		}
+		else if(op_menu_principal == 3){
+			mensalidades();
+		}
+		else if(op_menu_principal == 4){
+			pagamentos();
+		}
+		else if(op_menu_principal == 5){
+			registrar_acesso();
+		}
+		else if(op_menu_principal == 0){
+			printf("Módulo de Encerramento!");
+			entrada = false;
+		}
+		else{
+			printf("Entrada inválida, tente novamente:\n");
+		}
+    }
+    printf("\nSISTEMA ENCERRADO!");
 
     return 0;
 }
