e46d70196c6e5610f277f803ada7462610cb037f
Melque Rodrigues <melquetrindade654@gmail.com>
2022-09-04 15:37:00 -0300

    criação do esqueleto do código principal

--- semana_02/arq_semana_02.c
+++ semana_02/arq_semana_02.c
@@ -24,7 +24,40 @@ int op_menu_reg_acesso;
 
 int main(void){
 
+	setlocale(LC_ALL,"Portuguese");
+    #define true 1
+    #define false 0
+    entrada = true;
 
+    while(entrada != false){
+        op_menu_principal = menu_principal();
+        if(op_menu_principal == 1){
+            system("clear||cls");
+		}
+		else if(op_menu_principal == 2){
+			system("clear||cls");	
+		}
+		else if(op_menu_principal == 3){
+			system("clear||cls");
+		}
+		else if(op_menu_principal == 4){
+			system("clear||cls");
+		}
+		else if(op_menu_principal == 5){
+			system("clear||cls");
+		}
+		else if(op_menu_principal == 6){
+			system("clear||cls");
+		}
+		else if(op_menu_principal == 7){
+			system("clear||cls");
+		}
+		else{
+			system("clear||cls");
+			printf("Entrada inválida, tente novamente:\n");
+		}
+    }
+    printf("SISTEMA ENCERRADO!\n");
     
 
     return 0;
