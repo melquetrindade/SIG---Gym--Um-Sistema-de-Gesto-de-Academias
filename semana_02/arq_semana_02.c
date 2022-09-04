7568e2c3fdd38a9d55f620cedb3558fe60bebb1a
Melque Rodrigues <melquetrindade654@gmail.com>
2022-09-04 16:21:22 -0300

    corpo do main

--- semana_02/arq_semana_02.c
+++ semana_02/arq_semana_02.c
@@ -1,4 +1,54 @@
-<<<<<<< HEAD
-=======
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include "biblioteca.h"
 
->>>>>>> f9f07ee08466e3eec8d1b1dd36d996b54d392d4d
+// Variáveis
+int entrada;
+int op_menu_principal;
+int op_menu_clientes;
+int op_menu_funcionarios;
+int op_menu_mensalidades;
+int op_menu_pagamentos;
+int op_menu_reg_acesso;
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    #define true 1
+    #define false 0
+    entrada = true;
+
+    while(entrada != false){
+        op_menu_principal = menu_principal();
+        if(op_menu_principal == 1){
+            system("clear||cls");
+        }
+        else if(op_menu_principal == 2){
+            system("clear||cls");
+        }
+        else if(op_menu_principal == 3){
+            system("clear||cls");
+        }
+        else if(op_menu_principal == 4){
+            system("clear||cls");
+        }
+        else if(op_menu_principal == 5){
+            system("clear||cls");
+        }
+        else if(op_menu_principal == 6){
+            system("clear||cls");
+        }
+        else if(op_menu_principal == 7){
+            system("clear||cls");
+            printf("Módulo de Encerramento!\n");
+            entrada = false;
+        }
+        else{
+            system("clear||cls");
+            printf("Entrda inválida, tente novamente:\n");
+        }
+    }
+    printf("SISTEMA ENCERRADO!\n");
+
+    return 0;
+}
---
+++ semana_02/biblioteca.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// Assinatura das funções
+int menu_principal(void);
+int clientes(void);
+int funcionario(void);
+int pagamentos(void);
+int mensalidades(void);
+int registrar_acesso(void);
+int verifica_opcao(int opcao);
+void tela_sobre_o_tema(void);
+void tela_equipe(void);
