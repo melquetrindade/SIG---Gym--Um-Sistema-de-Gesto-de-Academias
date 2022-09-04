cf06a75f4b29b580137a96a64c2598ea71afb836
Melque Rodrigues <melquetrindade654@gmail.com>
2022-09-04 17:03:21 -0300

    Update biblioteca.h

--- semana_02/biblioteca.h
+++ semana_02/biblioteca.h
@@ -12,3 +12,29 @@ int registrar_acesso(void);
 int verifica_opcao(int opcao);
 void tela_sobre_o_tema(void);
 void tela_equipe(void);
+
+int menu_principal(void){ // Tela principal
+	setlocale(LC_ALL,"Portuguese");
+    char op[3];
+
+    printf("==============================================================\n");
+    printf("|                       MENU PRINCIPAL                       |\n");
+    printf("==============================================================\n");
+    printf("|                        BEM - VINDO                         |\n");
+    printf("--------------------------------------------------------------\n");
+    printf("|      ------------     ----------------     --------------- |\n");
+    printf("|    1-| CLIENTES |   2-| FUNCIONÁRIOS |   3-| MENSALIDADE | |\n");
+    printf("|      ------------     ----------------     --------------- |\n");
+    printf("|      --------------      --------------------              |\n");
+    printf("|    4-| PAGAMENTOS |    5-| REGISTRAR ACESSO |              |\n");
+    printf("|      --------------      --------------------              |\n");
+    printf("|      ------------------------------------     -----------  |\n");
+    printf("|    6-| SOBRE O PROGRAMA/DESENVOLVEDORES |   7-|  SAIR   |  |\n");
+    printf("|      ------------------------------------     -----------  |\n");
+    printf("==============================================================\n");
+    printf("\n");
+    printf(">>> ESCOLHA UMA OPÇÃO: "); fgets(op, 3, stdin); fflush(stdin);
+    printf("\n");
+    int x = atoi(op);
+    return x;
+}
