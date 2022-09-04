a855d29813d23b7727d9d06ef079f4b445e3e5bb
Melque Rodrigues <melquetrindade654@gmail.com>
2022-09-04 15:11:15 -0300

    refazendo semana02

--- semana_02/arq_semana_02.c
+++ semana_02/arq_semana_02.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-#include "biblioteca.h"
+
 
 ///////////////////////////////////////////////////////////////////////////////
 ///             Universidade Federal do Rio Grande do Norte                 ///
@@ -13,42 +13,9 @@
 ///                                Semana 02                                ///
 ///////////////////////////////////////////////////////////////////////////////
 
-int entrada;
-int op_menu_principal;
 
 int main(void){
     
-    setlocale(LC_ALL,"Portuguese");
-    #define true 1
-    #define false 0
-    entrada = true;
-    
-    while(entrada != false){
-        op_menu_principal = menu_principal();
-        if(op_menu_principal == 1){
-        	clientes();
-		}
-		else if(op_menu_principal == 2){
-			funcionarios();
-		}
-		else if(op_menu_principal == 3){
-			mensalidades();
-		}
-		else if(op_menu_principal == 4){
-			pagamentos();
-		}
-		else if(op_menu_principal == 5){
-			registrar_acesso();
-		}
-		else if(op_menu_principal == 0){
-			printf("Módulo de Encerramento!");
-			entrada = false;
-		}
-		else{
-			printf("Entrada inválida, tente novamente:\n");
-		}
-    }
-    printf("\nSISTEMA ENCERRADO!");
 
     return 0;
 }
