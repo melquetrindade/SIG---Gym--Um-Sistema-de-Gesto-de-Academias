bc6ae5133926010ac0fcd09abe97d21a95809a5e
Melque Rodrigues <melquetrindade654@gmail.com>
2022-09-08 22:26:19 -0300

    atualização de entrada dados

--- biblioteca.h
+++ biblioteca.h
@@ -237,26 +237,26 @@ void cadastrar_clientes(void){
     printf("\t|         Módulo de Cadastro      |\n");
     printf("\t===================================\n");
 
-    char cpf[11];
+    char cpf[30];
     char nome[100];
     char email[100];
-    char fone[10];
-    char data_nas[12];
+    char fone[30];
+    char data_nas[30];
 
-    printf("\n\tCPF:(APENAS NÚMEROS)>>> "); fgets(cpf, 11, stdin); fflush(stdin);
-    printf("\n\tNOME: "); fgets(nome, 100, stdin); fflush(stdin);
-    printf("\n\tE-MAIL: "); fgets(email, 100, stdin); fflush(stdin);
-    printf("\n\tTELEFONE:(APENAS NÚMEROS)>>> "); fgets(fone, 10, stdin); fflush(stdin);
-    printf("\n\tDATA DE NASCIMENTO:(dd/mm/aaaa)>>> "); fgets(data_nas, 12, stdin); fflush(stdin);
+    printf("\n\tCPF:(APENAS NÚMEROS)>>> "); fgets(cpf, 30, stdin); fflush(stdin);
+    printf("\tNOME: "); fgets(nome, 100, stdin); fflush(stdin);
+    printf("\tE-MAIL: "); fgets(email, 100, stdin); fflush(stdin);
+    printf("\tTELEFONE:(APENAS NÚMEROS)>>> "); fgets(fone, 30, stdin); fflush(stdin);
+    printf("\tDATA DE NASCIMENTO:(dd/mm/aaaa)>>> "); fgets(data_nas, 30, stdin); fflush(stdin);
 
     system("clear||cls");
 
-    printf("\tCLIENTE CADASTRADO COM SUCESSO!");
-    printf("\n\n\tCPF: %s", cpf);
-    printf("\n\tNOME: %s", nome);
-    printf("\n\tE-MAIL: %s", email);
-    printf("\n\tTELEFONE: %s", fone);
-    printf("\n\tDATA DE NASCIMENTO: %s", data_nas);
+    printf("\tCLIENTE CADASTRADO COM SUCESSO!\n");
+    printf("\n\tCPF: %s", cpf);
+    printf("\tNOME: %s", nome);
+    printf("\tE-MAIL: %s", email);
+    printf("\tTELEFONE: %s", fone);
+    printf("\tDATA DE NASCIMENTO: %s", data_nas);
     
     printf("\n\tPresione <ENTER> para voltar ao menu principal >>> ");
     getchar();
