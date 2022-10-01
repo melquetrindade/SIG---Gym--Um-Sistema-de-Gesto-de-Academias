4d6c29eaff6600ebbe8f7f57d5aac95be1540f44
Melque Rodrigues <melquetrindade654@gmail.com>
2022-10-01 11:22:30 -0300

    adicionando as funções no aquivo.c e arquivo.h na pasta de funcionários

---
+++ bibliotecas/funcionarios/funcionarios.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void cadastrar_funcionario(void){ // Função de cadastrar funcionários
+    system("clear||cls");
+
+    printf("\t===================================\n");
+    printf("\t|         Módulo de Cadastro      |\n");
+    printf("\t===================================\n");
+    printf("\t| CPF:(APENAS NÚMEROS)            |\n");
+    printf("\t| NOME COMPLETO:                  |\n");
+    printf("\t| E-MAIL:                         |\n");
+    printf("\t| TELEFONE:(APENAS NÚMEROS)       |\n");
+    printf("\t| DATA DE NASCIMENTO:(dd/mm/aaaa) |\n");
+    printf("\t===================================\n");
+
+    printf("\n\tPresione <ENTER> para voltar ao menu principal >>> ");
+    getchar();
+    system("clear||cls");
+}
+
+void deletar_funcionario(void){ // Função de deletar funcionários
+    system("clear||cls");
+
+    printf("\t===================================\n");
+    printf("\t|         Módulo de Exclusão      |\n");
+    printf("\t===================================\n");
+    printf("\t| INFORME O CPF: (APENAS NÚMEROS) |\n");
+    printf("\t===================================\n");
+    printf("\n\tPresione <ENTER> para voltar ao menu principal >>> ");
+    getchar();
+    system("clear||cls");
+}
+
+void recuperar_funcionario(void){ // Função de recuperar funcionários
+    system("clear||cls");
+
+    printf("\t===================================\n");
+    printf("\t|         Módulo de Recuperar     |\n");
+    printf("\t===================================\n");
+    printf("\t| INFORME O CPF: (APENAS NÚMEROS) |\n");
+    printf("\t===================================\n");
+    printf("\n\tPresione <ENTER> para voltar ao menu principal >>> ");
+    getchar();
+    system("clear||cls");
+}
+
+void pesquisar_funcionario(void){ // Função de pesquisar funcionários
+    system("clear||cls");
+
+    printf("\t===================================\n");
+    printf("\t|         Módulo de Pesquisar     |\n");
+    printf("\t===================================\n");
+    printf("\t| INFORME O CPF: (APENAS NÚMEROS) |\n");
+    printf("\t===================================\n");
+    printf("\n\tPresione <ENTER> para voltar ao menu principal >>> ");
+    getchar();
+    system("clear||cls");
+}
+
+void atualizar_funcionario(void){ // Função de atualizar funcionários
+    system("clear||cls");
+
+    printf("\t===================================\n");
+    printf("\t|       Módulo de Atualização     |\n");
+    printf("\t===================================\n");
+    printf("\t| INFORME O CPF: (APENAS NÚMEROS) |\n");
+    printf("\t===================================\n");
+    printf("\n\tPresione <ENTER> para voltar ao menu principal >>> ");
+    getchar();
+    system("clear||cls");
+}
+
+void listar_funcionario(void){ // Função de listar funcionários
+    system("clear||cls");
+
+    printf("\t===================================\n");
+    printf("\t|         Módulo de Listagem      |\n");
+    printf("\t===================================\n");
+    printf("\t| FUNCIONÁRIO 1:                  |\n");
+    printf("\t| CPF: -------                    |\n");
+    printf("\t| NOME COMPLETO: --------         |\n");
+    printf("\t| E-MAIL: ---------               |\n");
+    printf("\t| TELEFONE:--------               |\n");
+    printf("\t| DATA DE NASCIMENTO: --------    |\n");
+    printf("\t|                                 |\n");
+    printf("\t| FUNCIONÁRIO 2:                  |\n");
+    printf("\t|              .                  |\n");
+    printf("\t|              .                  |\n");
+    printf("\t|              .                  |\n");
+    printf("\t===================================\n");
+    printf("\n\tPresione <ENTER> para voltar ao menu principal >>> ");
+    getchar();
+    system("clear||cls");
+}
---
+++ bibliotecas/funcionarios/funcionarios.h
@@ -0,0 +1,6 @@
+void cadastrar_funcionario(void);
+void deletar_funcionario(void);
+void recuperar_funcionario(void);
+void pesquisar_funcionario(void);
+void atualizar_funcionario(void);
+void listar_funcionario(void);
