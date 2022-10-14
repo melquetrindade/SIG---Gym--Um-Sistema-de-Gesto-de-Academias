89d651582fbee614a3743585df9f051f4f02a0ff
Melque Rodrigues <melquetrindade654@gmail.com>
2022-10-13 21:40:19 -0300

    pequenas mudanças nas funções

--- bibliotecas/fun_reutilizaveis/fun_reutilizaveis.h
+++ bibliotecas/fun_reutilizaveis/fun_reutilizaveis.h
@@ -1,7 +1,7 @@
 int valida_format_data(char *);
 void loop_de_validacao_data(char *);
-int bissexto(int );
-int dataValida(int , int , int );
+int bissexto(int);
+int dataValida(int , int , int);
 void divide_data_inteiro(char *, int *);
 void loop_cpf(char *);
 int valida_cpf(char *);
