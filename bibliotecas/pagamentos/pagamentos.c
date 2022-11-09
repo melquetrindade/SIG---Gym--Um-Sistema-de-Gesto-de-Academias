2f67256611cbc607c19a326eef9ed8bd2ce76322
Melque Rodrigues <melquetrindade654@gmail.com>
2022-11-08 21:28:52 -0300

    Atualizações e implementação de novas funções no módulo de pagamentos de funcionários

--- bibliotecas/pagamentos/pagamentos.c
+++ bibliotecas/pagamentos/pagamentos.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <string.h>
 #include "../fun_reutilizaveis/fun_reutilizaveis.h"
 #include "../menu_principal/menu_principal.h"
 #include "pagamentos.h"
+#include "../funcionarios/funcionarios.h"
+
+typedef struct salario Salario;
+
+char arq_salario[] = {"arq_salario.dat"};
+char arq_funcionario[] = {"arq_funcionario.dat"};
 
 void realizar_pagamentos(void){ // Função de realizar Pagamentos dos Funcionários
     system("clear||cls");
+    Salario *salario;
 
     printf("\t========================================\n");
     printf("\t|      Módulo de Registrar Salário     |\n");
@@ -16,26 +24,58 @@ void realizar_pagamentos(void){ // Função de realizar Pagamentos dos Funcioná
 
     printf("\n\tINFORME O CPF: (APENAS NÚMEROS) >>> "); fgets(cpf, 100, stdin); fflush(stdin);
     loop_cpf(cpf);
+    salario = busca_salario(arq_salario,cpf);
+    if(salario == NULL){
+        Funcionario *funcionario;
+        funcionario = busca_funcionario(arq_funcionario, cpf);
+        if(funcionario == NULL){
+            printf("\n\tERRO, NÃO EXISTE NENHUM FUNCIONÁRIO CADASTRADO NO SISTEMA COM ESTE CPF!");
+        }
+        else{
+            Salario *salario1;
+            salario1 = preenche_salario(funcionario->cpf,funcionario->nome, funcionario->salaraio);
+            printf("\n\tEFETUAMENTO DO PRIMEIRO SALÁRIO!");
+            printf("\n\n\tPRESIONE <ENTER> PARA CONFIRMAR O PARAGAMENTO >>> ");
+            getchar();
+            printf("\n\tSALÁRIO PAGO COM SUCESSO!");
+            salvar_no_arq_salario(salario1, arq_salario);
+            free(salario1);
+        }
+        free(salario);
+    }
+    else{
+        int atraso = cal_atraso(salario->data_pg[2], salario->data_pg[1], salario->data_pg[0]);
+        if(atraso == 1){
+            int dif_mes = 0;
+            cal_atraso_etp2(salario->data_pg[2], salario->data_pg[1], salario->data_pg[0], &dif_mes);
+            printf("\n\tSALÁRIO EM ATRASO! TOTAL DE MESES EM ATRASO: %d", dif_mes);
+            printf("\n\n\tPRESIONE <ENTER> PARA CONFIRMAR O PARAGAMENTO DE: %d/%d/%d >>> ",salario->prox_data[0],salario->prox_data[1], salario->prox_data[2]);
+            getchar();
+            confirma_salario(arq_salario,salario);
+            printf("\n\tMENSALIDADE REALIZADA COM SUCESSO!");
+        }
+        else{
+            printf("\n\tNÃO HÁ SALÁRIOS PENDENTES PARA ESTE FUNCIONÁRIO!");
+            printf("\n\tPRÓXIMO PAGAMENTO: %d/%d/%d", salario->prox_data[0],salario->prox_data[1],salario->prox_data[2]);
+        }
+    }
+    free(salario);
 
-    printf("\n\tPresione <ENTER> para voltar ao menu principal >>> ");
+    printf("\n\n\tPresione <ENTER> para voltar ao menu principal >>> ");
     getchar();
     system("clear||cls");
 }
 
 void listar_pagamentos(void){ // Função de Listar os pagamentos dos funcionários
     system("clear||cls");
+    char status = 'v';
 
     printf("\t=======================================\n");
     printf("\t|     Módulo de Listar Pagamentos     |\n");
     printf("\t=======================================\n");
-    printf("\t| SALÁRIO 1:                          |\n");
-    printf("\t| NOME DO FUNCIONÁRIO: ----------     |\n");
-    printf("\t| ÚLTIMO PAGAMENTO REALIZADO: ------  |\n");
-    printf("\t|                                     |\n");
-    printf("\t| SALÁRIO 2:                          |\n");
-    printf("\t|                 .                   |\n");
-    printf("\t|                 .                   |\n");
-    printf("\t=======================================\n");
+
+    ler_arquivo_salario(arq_salario,status);
+
     printf("\n\tPresione <ENTER> para voltar ao menu principal >>> ");
     getchar();
     system("clear||cls");
@@ -43,6 +83,7 @@ void listar_pagamentos(void){ // Função de Listar os pagamentos dos funcionár
 
 void pesquisar_pagamentos(void){ // Função de Pesquisar por pagamentos
     system("clear||cls");
+    Salario *salario;
 
     printf("\t======================================\n");
     printf("\t|   Módulo de Pesquisar Pagamentos   |\n");
@@ -52,6 +93,14 @@ void pesquisar_pagamentos(void){ // Função de Pesquisar por pagamentos
 
     printf("\n\tINFORME O CPF: (APENAS NÚMEROS) >>> "); fgets(cpf, 100, stdin); fflush(stdin);
     loop_cpf(cpf);
+    salario = pesquisa_salario(arq_salario, cpf);
+    if(salario == NULL){
+        printf("\n\tNÃO EXISTE NENHUM SALÁRIO CADASTRADO COM ESTE CPF NO SISTEMA!");
+    }
+    else{
+        exibe_salario(salario);
+    }
+    free(salario);
 
     printf("\n\tPresione <ENTER> para voltar ao menu principal >>> ");
     getchar();
@@ -60,6 +109,7 @@ void pesquisar_pagamentos(void){ // Função de Pesquisar por pagamentos
 
 void deletar_pagamentos(void){ // Função de deletar pagamentos 
     system("clear||cls");
+    Salario *salario;
 
     printf("\t======================================\n");
     printf("\t|    Módulo de Deletar Pagamentos    |\n");
@@ -69,6 +119,14 @@ void deletar_pagamentos(void){ // Função de deletar pagamentos
 
     printf("\n\tINFORME O CPF: (APENAS NÚMEROS) >>> "); fgets(cpf, 100, stdin); fflush(stdin);
     loop_cpf(cpf);
+    salario = pesquisa_salario(arq_salario, cpf);
+    if(salario == NULL){
+        printf("\n\tNÃO EXISTE NENHUM SALÁRIO CADASTRADO COM ESTE CPF NO SISTEMA!");
+    }
+    else{
+        deleta_salario(arq_salario, salario);
+    }
+    free(salario);
 
     printf("\n\tPresione <ENTER> para voltar ao menu principal >>> ");
     getchar();
@@ -77,19 +135,216 @@ void deletar_pagamentos(void){ // Função de deletar pagamentos
 
 void pagamentos_pendentes(void){ // Função de listar apenas os salários atrasados
     system("clear||cls");
+    char status = 'x';
 
     printf("\t=======================================\n");
     printf("\t|    Módulo de Pagamentos Pendentes   |\n");
     printf("\t=======================================\n");
-    printf("\t| SALÁRIO 1:                          |\n");
-    printf("\t| NOME DO CLIENTE: ----------         |\n");
-    printf("\t| ÚLTIMO PAGAMENTO REALIZADO: ------  |\n");
-    printf("\t|                                     |\n");
-    printf("\t| SALÁRIO 2:                          |\n");
-    printf("\t|                 .                   |\n");
-    printf("\t|                 .                   |\n");
-    printf("\t=======================================\n");
+
+    ler_arquivo_salario(arq_salario,status);
+
     printf("\n\tPresione <ENTER> para voltar ao menu principal >>> ");
     getchar();
     system("clear||cls");
+}
+
+Salario* busca_salario(char *arquivo, char *cpf_busca){
+    FILE *arq;
+    int abriu = 1;
+    arq = fopen(arquivo, "rb");
+    if(arq == NULL){
+        abriu = 0;
+    }
+    if(abriu == 1){
+        Salario *salario;
+        salario = (Salario*) malloc(sizeof(Salario));
+        while(!feof(arq)){
+            if(fread(salario, sizeof(Salario), 1, arq)){
+                if((strcmp(salario->cpf, cpf_busca) == 0) && (salario->status == 'v')){
+                    fclose(arq);
+                    return salario;
+                }
+            }
+        }
+        fclose(arq);
+        return NULL;
+    }
+    return NULL;
+}
+
+Salario* preenche_salario(char *cpf, char*nome, char *plano){
+    Salario *salario;
+    salario = (Salario*) malloc(sizeof(Salario));
+    strcpy(salario->cpf,cpf);
+    strcpy(salario->nome,nome);
+    strcpy(salario->salario,plano);
+    pegaData(salario->data_pg);
+    cal_prox_pagamento(salario->data_pg[2],salario->data_pg[1], salario->data_pg[0], salario->prox_data);
+    salario->status = 'v';
+    return salario;
+}
+
+void salvar_no_arq_salario(const Salario *salario, char *arquivo){
+    FILE *arq;
+    arq = fopen(arquivo, "ab");
+    if (arq == NULL){
+        printf("ERRO NA ABERTURA DO ARQUIVO!\n");
+        exit(1);
+    }
+    fwrite(salario, sizeof(Salario), 1, arq);
+    fclose(arq);
+}
+
+void confirma_salario(char *arquivo, Salario *salario_novo){
+    Salario *salario_teste;
+    FILE *arq;
+    arq = fopen(arquivo, "r+b");
+    if (arq == NULL) {
+        printf("\n\tERRO NA ABERTURA DO ARQUIVO!\n");
+        exit(1);
+    }
+    salario_teste = (Salario*) malloc(sizeof(Salario));
+    int achou = 0;
+    while((!feof(arq)) && (achou == 0)){
+        fread(salario_teste, sizeof(Salario), 1, arq);
+        if((strcmp(salario_teste->cpf, salario_novo->cpf) == 0) && (salario_teste->status != 'x')) {
+            achou = 1;
+            strcpy(salario_teste->cpf,salario_novo->cpf);
+            strcpy(salario_teste->nome,salario_novo->nome);
+            strcpy(salario_teste->salario,salario_novo->salario);
+            salario_teste->data_pg[0] = salario_novo->prox_data[2];
+            salario_teste->data_pg[1] = salario_novo->prox_data[1];
+            salario_teste->data_pg[2] = salario_novo->prox_data[0];
+            cal_prox_pagamento(salario_teste->data_pg[2],salario_teste->data_pg[1], salario_teste->data_pg[0],salario_teste->prox_data);
+            salario_teste->status = 'v';
+            fseek(arq, -1*sizeof(Salario), SEEK_CUR);
+            fwrite(salario_teste, sizeof(Salario), 1, arq);
+        }
+    }
+    fclose(arq);
+    free(salario_teste);
+}
+
+void ler_arquivo_salario(char *arquivo, char statu){
+    FILE *arq;
+    arq = fopen(arquivo, "rb");
+    if (arq == NULL){
+        printf("\n\tERRO NA ABERTURA DO ARQUIVO!\n");
+        exit(1);
+    }
+    Salario *salario1;
+    salario1 = (Salario*) malloc(sizeof(Salario));
+    int cont = 0;
+    while(!feof(arq)){
+        if(fread(salario1, sizeof(Salario),1,arq)){
+            if(salario1->status == statu){
+                printf("\n\tSALÁRIO %d:\n",cont+1);
+                printf("\n\tCPF: %s", salario1->cpf);
+                printf("\tNOME: %s", salario1->nome);
+                printf("\tPLANO: R$ %s", salario1->salario);
+                printf("\n\tÚLTIMO SALÁRIO PAGO: %d/%d/%d", salario1->data_pg[2],salario1->data_pg[1],salario1->data_pg[0]);
+                printf("\n\tPRÓXIMO PAGAMENTO: %d/%d/%d", salario1->prox_data[0], salario1->prox_data[1], salario1->prox_data[2]);
+                printf("\n\t===================================\n");
+                cont+=1;
+            }
+        }
+    }
+    if(cont == 0){
+        if(statu == 'v'){
+            printf("\n\tNÃO EXISTE NENHUM SALÁRIO NO SISTEMA!\n");
+        }
+        else{
+            printf("\n\tNÃO EXISTE NENHUM SALÁRIO ATRASADO NO SISTEMA!\n");
+        }
+    }
+    fclose(arq);
+    free(salario1);
+}
+
+Salario* pesquisa_salario(char *arquivo, char *cpf_busca){
+    FILE *arq;
+    Salario *salario;
+    arq = fopen(arquivo, "rb");
+    if(arq == NULL){
+        printf("\n\tERRO NA ABERTURA DO ARQUIVO!\n");
+        exit(1);
+    }
+    salario = (Salario*) malloc(sizeof(Salario));
+    while(!feof(arq)){
+        if(fread(salario, sizeof(Salario), 1, arq)){
+            if((strcmp(salario->cpf, cpf_busca) == 0) && (salario->status == 'v')){
+                fclose(arq);
+                return salario;
+            }
+        }
+    }
+    fclose(arq);
+    return NULL;
+}
+
+void exibe_salario(const Salario* salario){ // Função exibe o cliente cadastrado
+    printf("\n\tCPF: %s", salario->cpf);
+    printf("\tNOME: %s", salario->nome);
+    printf("\tPLANO: %s", salario->salario);
+    printf("\n\tÚLTIMO SALÁRIO PAGO: %d/%d/%d", salario->data_pg[2],salario->data_pg[1],salario->data_pg[0]);
+    printf("\n\tPRÓXIMO PAGAMENTO: %d/%d/%d", salario->prox_data[0],salario->prox_data[1],salario->prox_data[2]);
+}
+
+void deleta_salario(char *arquivo, Salario *salario){
+    Salario *salario_teste;
+    FILE *arq;
+    arq = fopen(arquivo, "r+b");
+    if (arq == NULL) {
+        printf("\n\tERRO NA ABERTURA DO ARQUIVO!\n");
+        exit(1);
+    }
+    salario_teste = (Salario*) malloc(sizeof(Salario));
+    int achou = 0;
+    while((!feof(arq)) && (achou == 0)) {
+        fread(salario_teste, sizeof(Salario), 1, arq);
+        if((strcmp(salario_teste->cpf, salario->cpf) == 0) && (salario_teste->status != 'x')) {
+            achou = 1;
+            int atraso = cal_atraso(salario_teste->data_pg[2], salario_teste->data_pg[1], salario_teste->data_pg[0]);
+            if(atraso == 1){
+                char op[15];
+                int dif_mes = 0;
+                cal_atraso_etp2(salario_teste->data_pg[2], salario_teste->data_pg[1], salario_teste->data_pg[0], &dif_mes);
+                printf("\n\tO SEGUINTE SALAŔIO SERÁ EXCLUÍDO:");
+                exibe_salario(salario_teste);
+                printf("\n\tOBS: O FUNCIONÁRIO TEM SALÁRIO EM ATRASO! TOTAL DE MESES EM ATRASO: %d", dif_mes);
+                printf("\n\n\tDESEJA REALMENTE EXCLUIR? 1-(SIM) OU 0-(NÃO) >>> "); fgets(op,15,stdin); fflush(stdin);
+                confirma_exclucao_salario(salario_teste, op, arq);
+            }
+            else{
+                char op[15];
+                printf("\n\tO SEGUINTE SALÁRIO SERÁ EXCLUÍDO:");
+                exibe_salario(salario_teste);
+                printf("\n\tOBS: NÃO HÁ SALÁRIO PENDENTE PARA ESTE FUNCIONÁRIO!");
+                printf("\n\n\tDESEJA REALMENTE EXCLUIR? 1-(SIM) OU 0-(NÃO) >>> "); fgets(op,15,stdin); fflush(stdin);
+                confirma_exclucao_salario(salario_teste, op, arq);
+            }
+        }
+    }
+    fclose(arq);
+    free(salario_teste);
+}
+
+void confirma_exclucao_salario(Salario *salario, char *op, FILE *arq){
+    int op1 = atoi(op);
+    while((op1 < 0) || (op1 > 1)){
+        system("clear||cls");
+        printf("\n\tOPÇÃO INVÁLIDA! TENTE NOVAMENTE:");
+        char op_aux[10];
+        printf("\n\n\tDESEJA REALMENTE EXCLUIR? 1-(SIM) OU 0-(NÃO) >>> "); fgets(op_aux, 10, stdin); fflush(stdin);
+        op1 = atoi(op_aux);
+    }
+    if(op1 == 1){
+        salario->status = 'x';
+        fseek(arq, -1*sizeof(Salario), SEEK_CUR);
+        fwrite(salario, sizeof(salario), 1, arq);
+        printf("\n\tSALAŔIO EXCLUÍDO COM SUCESSO!\n");
+    }
+    else{
+        printf("\n\tA EXCLUÇÃO FOI CANCELADA!");
+    }
 }
--- bibliotecas/pagamentos/pagamentos.h
+++ bibliotecas/pagamentos/pagamentos.h
@@ -1,5 +1,25 @@
+typedef struct salario Salario;
+
+struct salario{
+    char cpf[100];
+    char nome[100];
+    char salario[20];
+    int data_pg[6];
+    int prox_data[3];
+    char status;
+};
+
 void realizar_pagamentos(void);
 void listar_pagamentos(void);
 void pesquisar_pagamentos(void);
 void deletar_pagamentos(void);
 void pagamentos_pendentes(void);
+Salario* busca_salario(char*, char*);
+Salario* preenche_salario(char*, char*, char*);
+void salvar_no_arq_salario(const Salario*, char*);
+void confirma_salario(char*, Salario*);
+void ler_arquivo_salario(char*, char);
+Salario* pesquisa_salario(char*, char*);
+void exibe_salario(const Salario*);
+void deleta_salario(char*, Salario*);
+void confirma_exclucao_salario(Salario*, char*, FILE*);
